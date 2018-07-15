a4c8835ebf4ba972f8900ce19646a9e4fb3c1fe7
Pramilakhadka <40908835+Pramilakhadka@users.noreply.github.com>
2018-07-15 08:46:43 +0545

    July 7

---
+++ july 7/Python assingment 1.py
@@ -0,0 +1,25 @@
+#Question 1
+
+v=25 
+u=0 
+t=10
+a=(v-u)/t
+print("Acceleration=",a)
+
+#question no 2
+a=str(input("Enter your first name"))
+b=str(input("Enter your last name"))
+Name= a+b
+print ("your name is:",Name)
+
+#Question no 3
+y=input("Enter a string")
+a=y*10
+print(a)
+
+#Question n0.4
+
+print(type(4))
+print(str(45))
+print(eval("2+3"))
+
